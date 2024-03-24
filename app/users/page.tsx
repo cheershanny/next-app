65cda776ce6d4ee36bb4d698d7f5e590d076d147
Hanny Tran <cheershanny@gmail.com>
2024-03-24 16:07:11 +0000

    formatting

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,16 +3,20 @@ import UsersTable from "./UsersTable";
 import Link from "next/link";
 
 interface Props {
-  searchParams: {sortOrder: string}
+  searchParams: { sortOrder: string };
 }
-const UsersPage = async ({searchParams: {sortOrder}}: Props) => {
-    return <>
-  <h1>Users</h1>
-  <Link href="/users/new" className='btn'>New User</Link>
-  <Suspense fallback={<p>Loading...</p>}>
-  <UsersTable sortOrder={sortOrder} />
-  </Suspense>
-  </>;
+const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
+  return (
+    <>
+      <h1>Users</h1>
+      <Link href="/users/new" className="btn">
+        New User
+      </Link>
+      <Suspense fallback={<p>Loading...</p>}>
+        <UsersTable sortOrder={sortOrder} />
+      </Suspense>
+    </>
+  );
 };
 
 export default UsersPage;
