9d9d2ef2f36c660be31e737eb96374ed6bd2c306
Hanny Tran <cheershanny@gmail.com>
2024-03-27 14:53:47 +0000

    replace hardcode data with prisma data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,27 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
 interface Props {
   params: { id: number };
 }
-export function GET(request: NextRequest, { params }: Props) {
-  if (params.id > 10)
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
-  return NextResponse.json({ id: 1, name: "Han" });
+  return NextResponse.json(user);
 }
 
-export async function PUT(request: NextRequest, { params }: Props) {
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
   const body = await request.json();
-  const validation = schema.safeParse(body)
+  const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
-return NextResponse.json({id: 1, name: body.name})
+  return NextResponse.json({ id: 1, name: body.name });
 }
 
-export function DELETE(request: NextRequest, {params}: Props) {
-    if (params.id>10)
-    return NextResponse.json({error: "user not found"}, {status: 404})
-return NextResponse.json({message: "success!"}, {status: 200})
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "user not found" }, { status: 404 });
+  return NextResponse.json({ message: "success!" }, { status: 200 });
 }
