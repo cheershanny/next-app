69aeccc08cb88fd8dba44cb5e059b9792b463d90
Hanny Tran <cheershanny@gmail.com>
2024-03-31 09:05:10 +0100

    add 2 new links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,6 +7,8 @@ const NavBar = () => {
       <Link href="/" className="mr-5" >Home</Link>
       <Link href="/users" className='mx-5'>Users</Link>
       <Link href="/products" className='mx-5'>Products</Link>
+      <Link href="/upload" className='mx-5'>Upload</Link>
+      <Link href="/api/auth/signin" className='mx-5'>Login</Link>
     </div>
   )
 }
