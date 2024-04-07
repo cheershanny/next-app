cc2c35d9f82a48f0e6b4cdf6d14acf6873f294d4
Hanny Tran <cheershanny@gmail.com>
2024-04-07 18:26:21 +0100

    style with Tailwind

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Html, Body, Container, Text, Link, Preview } from '@react-email/components'
+import React from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Tailwind,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
 
-const WelcomeTemplate = ({name}: {name: string}) => {
+const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
-        <Preview>Welcome onboard</Preview>
+      <Preview>Welcome onboard</Preview>
+      <Tailwind>
         <Body>
-            <Container>
-                <Text>Hello {name}</Text>
-                <Link href='https://github.com/'>my github home</Link>
-            </Container>
+          <Container className="bg-sky-500/100">
+            <Text className="text-3xl font-bold">Hello {name}</Text>
+            <Link href="https://github.com/">my github home</Link>
+          </Container>
         </Body>
+      </Tailwind>
     </Html>
-  )
-}
+  );
+};
 
-export default WelcomeTemplate
+export default WelcomeTemplate;
