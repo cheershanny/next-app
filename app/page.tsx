26d482a0957a011ab3f7aa56219e89633903f3a6
Hanny Tran <cheershanny@gmail.com>
2024-04-08 15:40:10 +0100

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import hanny from "@/public/images/hanny-after-seeing-food.jpeg";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <h1>
+        Hello{" "}
+        {session ? <span>{session.user!.name}</span> : <span>amigo! 🦩</span>}
+      </h1>
+      <Image src={hanny} alt="Hanny is happy when food arrived" />
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="react cover"
+        width={300}
+        height={100}
+      />
     </main>
   );
 }
